e3377deab73daaf86e3238c0c0bce88321fe3ec0
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-06-02 13:10:45 +0300

    Something else...

--- Models/PhoneBookContext.cs
+++ Models/PhoneBookContext.cs
@@ -17,5 +17,31 @@ namespace phonebook.Models
 
         private MySqlConnection GetConnection() => new MySqlConnection(ConnectionString);
 
+        public List<PhoneNumber> GetAllNumbers()
+        {
+            List<PhoneNumber> list = new List<PhoneNumber>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Album where id < 10", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new PhoneNumber()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            LastName = reader["lastname"].ToString(),
+                            FirstName = reader["firstname"].ToString(),
+                            Address = reader["address"].ToString(),
+                            PhoneNum = reader["phonenumber"].ToString(),
+                        });
+                    }
+                }
+            }
+            return list;
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using phonebook.Models;
 
 namespace phonebook
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -25,23 +24,35 @@ namespace phonebook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // Add framework services.  
+            services.AddMvc();
+            services.Add(new ServiceDescriptor(typeof(PhoneBookContext), new PhoneBookContext(Configuration.GetConnectionString("DefaultConnection"))));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //app.UseBrowserLink();
             }
             else
             {
-                app.UseHsts();
+                app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }

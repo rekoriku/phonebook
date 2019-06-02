08ca11bd9487281b588ea3cff6de4f263669e978
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-06-02 13:18:21 +0300

    Something different...

---
+++ Controllers/PhoneNumberController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using phonebook.Models;
+
+namespace phonebook.Controllers
+{
+    public class PhoneNumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            PhoneBookContext context = HttpContext.RequestServices.GetService(typeof(phonebook.Models.PhoneBookContext)) as PhoneBookContext;
+
+            return View(context.GetAllNumbers());
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace phonebook
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=PhoneNumber}/{action=Index}/{id?}");
             });
         }
     }

0e776575af21e4705bde637d8eb75c1f547b3281
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-06-02 12:47:41 +0300

    Something....

---
+++ Models/PhoneBookContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phonebook.Models
+{
+    public class PhoneBookContext
+    {
+        public string ConnectionString { get; set; }
+
+        public PhoneBookContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+}
---
+++ Models/PhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phonebook.Models
+{
+    public class PhoneNumber
+    {
+        private PhoneBookContext context;
+
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNum { get; set; }
+    }
+}
